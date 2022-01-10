b5af034d156cbd2dc53f7b6c72b4c32add029d0f
SairovARG <pmrovira92@gmail.com>
2022-01-09 22:30:40 -0300

    [ADD] making the home dinamic

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,5 +1,14 @@
 const path = require('path');
+const items = require('../database/items');
+const collections = require('../database/collections');
 
 module.exports = {
-    index: (req, res) => res.render('home', {title: "Home"}),
+    index: (req, res) => {
+        let data = items.filter(el => el.sells > 10).sort((a ,b) => b.sells - a.sells);
+        return res.render('home', {
+            title: "Home",
+            items: data,
+            collections
+        })
+    },
 }
