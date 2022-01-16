891a42808134903c3f2ccb453bcd5d88773cfd55
SairovARG <pmrovira92@gmail.com>
2022-01-16 12:58:15 -0300

    [FEAT] adapting project for new methods and add 404 error page logic

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
+
+// Routes import
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
+// Puerto
 const puerto = process.env.PORT || 3000;
+
+// Carpeta Pública
 const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+// Parseamos nuestros datos para leer el body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Override para habilitar métodos PUT y DELETE
+app.use(methodOverride('_method'));
+
+// Views config
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
 
 
-app.set('view engine', 'ejs');
-
-app.use(express.static(publicPath));
+// Routers
 
 app.use('/', mainRoutes);
+app.use('/', userRoutes);
 app.use('/shop',shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).render('404')
+})
 
+// Lanzamiento del servidor
 
 app.listen(puerto, () => console.log('Servidor corriendo en puerto ' + puerto));
